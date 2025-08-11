e5094c825518ed670b7b099594d3d6ca6df98cca
Ermiyas Dagnachew <ermiyas.dagnachew@gmail.com>
2025-08-11 17:01:28 +0300

    update background color in Layout component

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -8,7 +8,7 @@ export const Layout: React.SFC<LayoutProps> = (props) => {
   return (
     <div
       className="min-h-screen w-full grid grid-cols-12 gap-4"
-      style={{ backgroundColor: "#EBECF8FF" }}
+      style={{ backgroundColor: "#FCD5CEFF" }}
     >
       {props.children}
     </div>
